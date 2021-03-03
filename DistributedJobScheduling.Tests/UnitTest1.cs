bb5b02066c1768b15988ae0c7b7d4b40710d2595
Jacopo Libè <jacopo.libe@gmail.com>
2021-03-03 16:49:48 +0100

    Added unit testing project for virtual sync logic testing

---
+++ DistributedJobScheduling.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Communication;
+using Xunit;
+
+namespace DistributedJobScheduling.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
