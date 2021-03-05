9c14245f754cec68685788b9271db9583a8b966f
Mirko <mirko.andena@gmail.com>
2021-03-05 17:33:09 +0100

    Scalar clock on messages

--- DistributedJobScheduling/Communication/Basic/Message.cs
+++ DistributedJobScheduling/Communication/Basic/Message.cs
@@ -3,8 +3,31 @@ using Newtonsoft.Json;
 
 namespace DistributedJobScheduling.Communication.Basic
 {
+    /// <summary>
+    /// Implement the Scalar Clock
+    /// </summary>
     public abstract class Message
     {
+        private static int _idCount = 0;
+        private string _messageID;
+        private string _isResponseOf;
+
+        public Message()
+        {
+            _messageID = $"{Workers.Instance.Me.ID}_{_idCount}";
+            _idCount++;
+        }
+
+        /// <summary>
+        /// Create a message that is the response of another message
+        /// </summary>
+        public Message(Message message) : this()
+        {
+            _isResponseOf = message._messageID;
+        }
+
+        public bool IsTheExpectedMessage(Message previous) => _isResponseOf == previous._messageID;
+
         public byte[] Serialize()
         {
             string json = JsonConvert.SerializeObject(this);
