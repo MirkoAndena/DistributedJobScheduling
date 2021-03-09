b21240a3c32873cd3df71ef83abf6dc9a17c1a27
Jacopo Libè <jacopo.libe@gmail.com>
2021-03-09 18:41:36 +0100

    Added latency simulation to networkbus, completed theorical implementation of virtual synchrony

--- DistributedJobScheduling.Tests/Stubs/Communication/StubNetworkBus.cs
+++ DistributedJobScheduling.Tests/Stubs/Communication/StubNetworkBus.cs
@@ -10,6 +10,7 @@ namespace DistributedJobScheduling.Tests.Communication
     public class StubNetworkBus
     {
         private Dictionary<Node, StubNetworkManager> _networkMap;
+        public float LatencyDeviation {get; set;} = 1.0f;
         private Random _random;
 
         public StubNetworkBus(int randomSeed)
@@ -35,7 +36,7 @@ namespace DistributedJobScheduling.Tests.Communication
 
         public async Task SendTo(Node from, Node to, Message message, int timeout)
         {
-            await Task.Delay(_random.Next(5,17));
+            await Task.Delay(_random.Next((int)(5*LatencyDeviation),(int)(17*LatencyDeviation)));
 
             if(_networkMap.ContainsKey(to))
                 _networkMap[to].FakeReceive(from, message);
@@ -43,12 +44,17 @@ namespace DistributedJobScheduling.Tests.Communication
                 await Task.Delay(TimeSpan.FromSeconds(timeout));
         }
 
-        public async Task SendMulticast(Node from,Message message)
+        public async Task SendMulticast(Node from, Message message)
         {
-            var nodesToSendTo = _networkMap.Keys.Where(x => x != from).OrderBy(n => _random.Next());
+            var nodesToSendTo = _networkMap.Keys.Where(x => x != from).OrderBy(n => _random.Next()).AsParallel();
 
-            await Task.Delay(_random.Next(5,17));
-            nodesToSendTo.ForEach(x => _networkMap[x].FakeReceive(from, message));
+            await Task.Delay(_random.Next((int)(3*LatencyDeviation),(int)(5*LatencyDeviation)));
+
+            //Emulate latency and out of order receive
+            Parallel.ForEach(nodesToSendTo, async x => {
+                await Task.Delay(_random.Next((int)(5*LatencyDeviation),(int)(17*LatencyDeviation)));
+                _networkMap[x].FakeReceive(from, message);
+            });
         }
     }
 }
