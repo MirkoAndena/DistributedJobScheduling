5ab483ecef0e157246c0d1693745b395444d9b4f
Mirko <mirko.andena@gmail.com>
2021-03-19 15:51:44 +0100

    Scheleton of keep alive test

---
+++ DistributedJobScheduling.Tests/KeepAliveTest.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading.Tasks;
+using DistributedJobScheduling.Communication.Basic;
+using DistributedJobScheduling.LeaderElection.KeepAlive;
+using DistributedJobScheduling.Logging;
+using DistributedJobScheduling.VirtualSynchrony;
+using Xunit;
+
+namespace DistributedJobScheduling.DistributedStorage
+{
+    public class KeepAliveTest
+    {
+        IGroupViewManager _group;
+        ILogger _logger;
+        
+        public KeepAliveTest()
+        {
+
+        }
+
+        private void KillNode(Node node)
+        {
+            // TODO KILL!
+        }
+
+        [Fact]
+        public void WorkerFail()
+        {
+            Node diedNode = null;
+            bool someoneDies = false;
+            CoordinatorKeepAlive keepAlive = new CoordinatorKeepAlive(_group, _logger);
+            keepAlive.NodesDied += nodes =>
+            {
+                Assert.Equal(nodes.Count, 1);
+                Assert.Equal(diedNode, nodes[0]);
+                someoneDies = true;
+            };
+            keepAlive.Init();
+            keepAlive.Start();
+            Task.Delay(TimeSpan.FromSeconds(6)).ContinueWith(t => KillNode(diedNode));
+            Task.Delay(TimeSpan.FromSeconds(30)).ContinueWith(t => 
+            {
+                keepAlive.Stop();
+                Assert.True(someoneDies);
+            });
+        }
+
+        [Fact]
+        public void CoordinatorFail()
+        {
+            Node coordinator = null;
+            bool coordinatorDies = false;
+            WorkersKeepAlive keepAlive = new WorkersKeepAlive(_group, _logger);
+            keepAlive.CoordinatorDied += () =>
+            {
+                coordinatorDies = true;
+            };
+            keepAlive.Init();
+            keepAlive.Start();
+            Task.Delay(TimeSpan.FromSeconds(6)).ContinueWith(t => KillNode(coordinator));
+            Task.Delay(TimeSpan.FromSeconds(30)).ContinueWith(t => 
+            {
+                keepAlive.Stop();
+                Assert.True(coordinatorDies);
+            });
+        }
+    }
+}
