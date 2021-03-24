bb5a4633640c8c183580f57c77aaee497963d26e
Mirko <mirko.andena@gmail.com>
2021-03-24 17:34:23 +0100

    Nodes arg in start batch

--- DistributedJobScheduling/start_scenario.ps1
+++ DistributedJobScheduling/start_scenario.ps1
@@ -1,5 +1,10 @@
+$nodes = 10
+if ($args.count > 0) {
+    $nodes = $args[0]
+}
+
 $initiatorIndex = 0 #No Initiator is negative
-$nodeContainers = New-Object string[] 10
+$nodeContainers = New-Object string[] $nodes
 For ($i=0; $i -lt $nodeContainers.Count; $i++) {
     $nodeDirectory = $(Get-Location).tostring() + "/ExecutorsStorage/node_" + $i
     $toExecute = 'docker run --mount src="' + $nodeDirectory + '",target=/app/DataStore,type=bind --restart=on-failure -d distributed-job-scheduling ' + $i
