988ccb7477309ca97cc72c5553f6654b22f418e8
Jacopo Libè <jacopo.libe@gmail.com>
2021-03-24 16:14:33 +0100

    Removed unecessary part

--- DistributedJobScheduling/start_scenario.ps1
+++ DistributedJobScheduling/start_scenario.ps1
@@ -12,8 +12,6 @@ For ($i=0; $i -lt $nodeContainers.Count; $i++) {
 Write-Host -NoNewLine 'Press any key to kill...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-docker container stop $initiatorContainer
-docker container rm $initiatorContainer
 Foreach ($container in $nodeContainers)
 {
     docker container stop $container
